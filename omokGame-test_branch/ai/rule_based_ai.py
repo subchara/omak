6fd779f27c951a778a43f29a1661d803862d6ff8
subhub <ch2235.com@gmail.com>
2026-05-28 12:12:27 +0900

    Update rule_based_ai.py

--- omokGame-test_branch/ai/rule_based_ai.py
+++ omokGame-test_branch/ai/rule_based_ai.py
@@ -1,30 +1,44 @@
 import random
 import numpy as np
 
+BOARD_SIZE = 15
+
 WIN_SCORE = 100000
 BLOCK_WIN = 90000
+OPEN_FOUR = 50000
+DOUBLE_THREE = 10000
 OPEN_THREE = 3000
 CENTER_BONUS = 50
 CONNECT_BONUS = 10
 
 
+DIRECTIONS = [
+    (0, 1),
+    (1, 0),
+    (1, 1),
+    (1, -1),
+]
+
+
 def get_candidate_moves(board, radius=2):
-    size = len(board)
 
     stones = np.argwhere(board != 0)
 
     if len(stones) == 0:
-        return [(size // 2, size // 2)]
+        return [(BOARD_SIZE // 2, BOARD_SIZE // 2)]
 
     candidates = set()
 
     for r, c in stones:
+
         for dr in range(-radius, radius + 1):
             for dc in range(-radius, radius + 1):
+
                 nr = r + dr
                 nc = c + dc
 
-                if 0 <= nr < size and 0 <= nc < size:
+                if 0 <= nr < BOARD_SIZE and 0 <= nc < BOARD_SIZE:
+
                     if board[nr][nc] == 0:
                         candidates.add((nr, nc))
 
@@ -32,18 +46,22 @@ def get_candidate_moves(board, radius=2):
 
 
 class RandomAI:
+
     def select_move(self, board, player):
         return random.choice(get_candidate_moves(board))
 
 
 class WeakRuleAI:
+
     def select_move(self, board, player):
+
         moves = get_candidate_moves(board)
 
-        best_move = None
         best_score = -1e9
+        best_move = None
 
         for move in moves:
+
             score = evaluate_move(board, move, player)
 
             if score > best_score:
@@ -53,39 +71,19 @@ class WeakRuleAI:
         return best_move
 
 
-class StrongRuleAI(WeakRuleAI):
-    pass
+class StrongRuleAI:
 
+    def select_move(self, board, player):
 
-def evaluate_move(board, move, player):
-    r, c = move
+        moves = get_candidate_moves(board)
 
-    if board[r][c] != 0:
-        return -1e9
+        best_score = -1e9
+        best_move = None
 
-    score = 0
+        opponent = 3 - player
 
-    center = len(board) // 2
-    dist = abs(r - center) + abs(c - center)
+        for move in moves:
 
-    score += CENTER_BONUS - dist
+            r, c = move
 
-    directions = [
-        (0, 1),
-        (1, 0),
-        (1, 1),
-        (1, -1),
-    ]
-
-    temp = board.copy()
-    temp[r][c] = player
-
-    for dr, dc in directions:
-        nr = r + dr
-        nc = c + dc
-
-        if 0 <= nr < len(board) and 0 <= nc < len(board):
-            if temp[nr][nc] == player:
-                score += CONNECT_BONUS
-
-    return score
+    return False
